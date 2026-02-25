5a49aa4ad3fcd8b824080099103b2ba1b281f56b
HairyPloper <antoni.krstic@gmail.com>
2026-02-25 11:09:07 +0000

    Deploying to gh-pages from @ HairyPloper/voice-room-web@a085a9b4e880a3c3ac02f5cf3982157da7ea555e 🚀

--- dev/combined.js
+++ dev/combined.js
@@ -668,10 +668,14 @@ function handleCommand(text) {
       if (!prompt) {
         window.appendMessage(
           "Sistem",
-          "Upišite pitanje, npr: /ai Koliko je 2+2?",
+          "Format: /ai Koliko je 2+2?",
           "#ef4444",
         );
       } else {
+      //   chatRef.push({
+      //   username: "Sistem",
+      //   text: `${window.myUsername} pita: ${prompt}`
+      // });
         window.askAI(prompt);
       }
       return true;
@@ -1039,7 +1043,7 @@ window.askAI = async (prompt) => {
 
         chatRef.push({
           username: `🤖 AI (${modelName})`,
-          text: aiText,
+          text: `Pitanje: ${prompt}\nOdgovor: ${aiText}`,
           color: "#fbbf24",
           timestamp: Date.now(),
         });
